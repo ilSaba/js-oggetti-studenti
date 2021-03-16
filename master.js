525eee2d07ccc94b148b266ff796b05a99cf5ab5
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-16 19:03:38 +0100

    Esercizio completato

--- master.js
+++ master.js
@@ -2,9 +2,61 @@
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto. 
 
 var studente = {
-    "nome": "Lorenzo",
-    "cognome": "bernini",
-    "età": "14 anni"
+    nome: 'Lorenzo',
+    cognome: 'Bernini',
+    età: '14 anni',
 }
-console.log(studente);
 
+for (var key in studente) {
+    console.log(studente[key]);
+}
+
+
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+var studenti = [
+    {
+    nome: 'Lorenzo',
+    cognome: 'Bernini',
+    età: '14 anni',
+    },
+
+    {
+    nome: 'Roy',
+    cognome: 'Paci',
+    età: '10 anni',
+    },
+
+    {
+    nome: 'Andrea',
+    cognome: 'Sabatini',
+    età: '8 anni',
+    },
+
+    {
+    nome: 'Luca',
+    cognome: 'Bernini',
+    età: '29 anni',
+    }
+];
+
+for (var key in studenti) {
+    console.log(studenti[key]);
+}
+
+// Dare la possibilità all’utente, attraverso 3 prompt(),
+// di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+$("#button").click(function (){
+    var name = $("#name").val();
+    var surname = $("#surname").val();
+    var eta = $("#eta").val();
+
+    var studenti = {
+        name: name,
+        cognome: surname,
+        eta: eta,
+    }
+    console.log(studenti);
+})
