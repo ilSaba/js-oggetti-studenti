e1c325d955e722953c4d17b87c5d0e41ba56c17c
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-16 15:36:27 +0100

    Primo esercizio completato

---
+++ master.js
@@ -0,0 +1,10 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+//Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto. 
+
+var studente = {
+    "nome": "Lorenzo",
+    "cognome": "bernini",
+    "età": "14 anni"
+}
+console.log(studente);
+
